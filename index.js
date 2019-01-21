5d52871176cbaaebca15559a44d3c438abfce210
Aman <amsingh714@gmail.com>
2019-01-21 17:42:30 -0500

    able to GET users

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
-const knex = require('knex');
-const knexConfig = require('../knexfile.js');
+const knex = require("knex");
+const knexConfig = require("./knexfile.js");
 const db = knex(knexConfig.development);
 
 const server = express();
 
 server.use(express.json());
 
+server.get("/api/users", (req, res) => {
+  db("users")
+    .select("id", "username")
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json({ error: "The users could not be retrieved." });
+    });
+});
+
+server.listen(5000, () => console.log("Running on port 5000"));
