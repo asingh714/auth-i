917bb0356d626568a4bb1ccd68ce3f9768ceea7a
Aman <amsingh714@gmail.com>
2019-04-01 15:47:42 -0400

    able to log in

--- data/routes/auth-router.js
+++ data/routes/auth-router.js
@@ -17,16 +17,48 @@ router.post("/register", (req, res) => {
     db("users")
       .insert(user)
       .then(ids => {
-        const id = ids[0]
+        const id = ids[0];
         db("users")
-        .where({ id })
-        .first()
-        .then(user => {
-          res.status(201).json(user);
-        })
+          .where({ id })
+          .first()
+          .then(user => {
+            res.status(201).json(user);
+          });
       })
       .catch(error => {
-        res.status(500).json({ error: "There was an error while saving the user to the database." });
+        res
+          .status(500)
+          .json({
+            error: "There was an error while saving the user to the database."
+          });
+      });
+  }
+});
+
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+
+  if (!username || !password) {
+    res.status(400).json({
+      error: "Please provide a username and password"
+    });
+  } else {
+    db("users")
+      .where({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: `${user.username} is logged in.` });
+        } else {
+          res.status(401).json({ message: "You shall not pass!" });
+        }
+      })
+      .catch(error => {
+        res
+          .status(500)
+          .json({
+            error: "There was an error while saving the user to the database."
+          });
       });
   }
 });
